d39c2014978d0459919293667333bd1df029a8f8
Bert <1521224526@qq.com>
2025-01-22 17:38:12 +0800

    fix

--- src/main/java/com/inateck/Main.java
+++ src/main/java/com/inateck/Main.java
@@ -1,112 +1,90 @@
 package com.inateck;
 
 public class Main {
-    public static void main(String[] args) {
-        ScannerBle scanner = new ScannerBle();
-        int status = scanner.registerEventCallback((result) -> {
-            System.out.println(result);
+
+    static void connect(ScannerBle scanner, String deviceID) {
+        scanner.connect(deviceID);
+        scanner.checkCommunication(deviceID);
+        scanner.auth(deviceID);
+        scanner.setCodeCallback(deviceID, (result) -> {
+            System.out.println("code: " + result);
         });
-        if (status != 0) {
-            System.out.println("init failed: " + status);
-            System.exit(1);
+    }
+
+    static void getHardwareVersion(ScannerBle scanner, String deviceID) {
+        String hardwareVersion = scanner.getHardwareVersion(deviceID);
+        System.out.println("hardwareVersion: " + hardwareVersion);
+    }
+
+    static void getSoftwareVersion(ScannerBle scanner, String deviceID) {
+        String softwareVersion = scanner.getSoftwareVersion(deviceID);
+        System.out.println("softwareVersion: " + softwareVersion);
+    }
+
+    static void getSettingInfo(ScannerBle scanner, String deviceID) {
+        String settingInfo = scanner.getSettingInfo(deviceID, InateckDeviceType.ST75);
+        System.out.println("settingInfo: " + settingInfo);
+    }
+
+    static void quietMode(ScannerBle scanner, String deviceID) {
+        String closeVolume = "[{\"flag\":1001,\"value\":0}]";
+        String info = scanner.setSettingInfo(deviceID, closeVolume, InateckDeviceType.ST75);
+        scanner.beeOrShake(deviceID);
+        System.out.println("quiet Mode： " + info);
+    }
+
+    static void loudMode(ScannerBle scanner, String deviceID) {
+        String openVolume = "[{\"flag\":1001,\"value\":4}]";
+        String info = scanner.setSettingInfo(deviceID, openVolume, InateckDeviceType.ST75);
+        scanner.beeOrShake(deviceID);
+        System.out.println("loud Mode： " + info);
+    }
+
+    static void sleep(int time) {
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-        System.out.println("init success");
-        System.out.println("you can input command: > scan, > stop, > devices, > connect, > disconnect, > version, > battery, > software, > settingInfo, > closeVolume, > openVolume, > destroy");
-        while (true) {
-            String inputStr = System.console().readLine();
-            String[] cmd = inputStr.split(" ");
-            String start = cmd[0];
-            if (!start.equals(">")) {
-                System.out.println("Invalid command, example: > scan");
-                continue;
-            }
-            String method = cmd[1];
-            if (method == null) {
-                System.out.println("Invalid command, example: > scan");
-                continue;
-            }
-            if (method.equals("scan")) {
-                status = scanner.startScan();
-                System.out.println("status: " + status);
-            } else if (method.equals("stop")) {
-                status = scanner.stopScan();
-                System.out.println("status: " + status);
-            } else if (method.equals("devices")) {
-                String devices = scanner.getDevices();
-                System.out.println("devices: " + devices);
-            } else if (method.equals("connect")) {
-                String mac = cmd[2];
-                if (mac == null) {
-                    System.out.println("Invalid command, example: > connect fb556f1d-f919-2d4d-c98c-fcbe246af2e4");
-                    continue;
-                }
-                String device = scanner.connect(mac, (result) -> {
-                    System.out.println(result);
-                });
-                scanner.auth(mac);
-                System.out.println("device: " + device);
-            } else if (method.equals("disconnect")) {
-                String mac = cmd[2];
-                if (mac == null) {
-                    System.out.println("Invalid command, example: > disconnect fb556f1d-f919-2d4d-c98c-fcbe246af2e4");
-                    continue;
-                }
-                int device = scanner.disconnect(mac);
-                System.out.println("device: " + device);
-            } else if (method.equals("version")) {
-                String mac = cmd[2];
-                if (mac == null) {
-                    System.out.println("Invalid command, example: > version fb556f1d-f919-2d4d-c98c-fcbe246af2e4");
-                    continue;
-                }
-                String version = scanner.getHardwareVersion(mac);
-                System.out.println("version: " + version);
-            } else if (method.equals("battery")) {
-                String mac = cmd[2];
-                if (mac == null) {
-                    System.out.println("Invalid command, example:" + " > battery fb556f1d-f919-2d4d-c98c-fcbe246af2e4");
-                }
-                String battery = scanner.getBattery(mac);
-                System.out.println("battery: " + battery);
-            } else if (method.equals("software")) {
-                String mac = cmd[2];
-                if (mac == null) {
-                    System.out.println("Invalid command, example: > software fb556f1d-f919-2d4d-c98c-fcbe246af2e4");
-                    continue;
-                }
-                String software = scanner.getSoftwareVersion(mac);
-                System.out.println("software: " + software);
-            } else if (method.equals("settingInfo")) {
-                String mac = cmd[2];
-                if (mac == null) {
-                    System.out.println("Invalid command, example: > settingInfo fb556f1d-f919-2d4d-c98c-fcbe246af2e4");
-                    continue;
-                }
-                String settingInfo = scanner.getSettingInfo(mac);
-                System.out.println("settingInfo: " + settingInfo);
-            } else if (method.equals("closeVolume")) {
-                String mac = cmd[2];
-                if (mac == null) {
-                    System.out.println("Invalid command, example: > closeVolume fb556f1d-f919-2d4d-c98c-fcbe246af2e4");
-                    continue;
-                }
-                String closeVolume = "[{\"area\":\"3\",\"value\":\"0\",\"name\":\"volume\"}]";
-                String info = scanner.setSettingInfo(mac, closeVolume);
-                System.out.println("settingInfo: " + info);
-            } else if (method.equals("openVolume")) {
-                String mac = cmd[2];
-                if (mac == null) {
-                    System.out.println("Invalid command, example: > closeVolume fb556f1d-f919-2d4d-c98c-fcbe246af2e4");
-                    continue;
-                }
-                String closeVolume = "[{\"area\":\"3\",\"value\":\"4\",\"name\":\"volume\"}]";
-                String info = scanner.setSettingInfo(mac, closeVolume);
-                System.out.println("settingInfo: " + info);
-            } else if (method.equals("destroy")) {
-                scanner.destroy();
-            } else {
-                System.out.println("Invalid command, example: > scan");
-            }
-        }
+    }
+
+    public static void main(String[] args) {
+
+        ScannerBle scanner = new ScannerBle();
+        scanner.setDebug(1);
+        scanner.init();
+        scanner.waitAvailable();
+        
+        scanner.setDiscoverCallback((result) -> {
+            System.out.println("discover: " + result);
+        });
+        scanner.startScan();
+        // sleep 10s
+        sleep(10000);
+        scanner.stopScan();
+
+        String deviceID = "BluetoothLE#BluetoothLEf4:4e:fc:89:ee:4a-aa:2b:00:03:95:83";
+
+        connect(scanner, deviceID);
+
+        sleep(10000);
+
+        scanner.beeOrShake(deviceID);
+
+        sleep(5000);
+
+        getHardwareVersion(scanner, deviceID);
+
+        getSoftwareVersion(scanner, deviceID);
+
+        getSettingInfo(scanner, deviceID);
+
+        quietMode(scanner, deviceID);
+
+        sleep(5000);
+
+        loudMode(scanner, deviceID);
+
+        sleep(5000);
     }
 }
--- src/main/java/com/inateck/ScannerBle.java
+++ src/main/java/com/inateck/ScannerBle.java
@@ -3,25 +3,60 @@ package com.inateck;
 import com.inateck.ScannerJNA.CLibrary;
 import com.inateck.ScannerJNA.EventCallback;
 
+enum InateckDeviceType {
+    None(0),
+    Pro8(1),
+    ST45(2),
+    ST23(3),
+    ST91(4),
+    ST42(5),
+    ST54(6),
+    ST55(7),
+    ST73(8),
+    ST75(9),
+    ST43(10),
+    P7(11),
+    ST21(12),
+    ST60(13),
+    ST70(14),
+    P6(15),
+    ST35(16);
+
+    private final int value;
+
+    InateckDeviceType(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
+
 public class ScannerBle {
 
-    int registerEventCallback(EventCallback callback) {
-        int status = CLibrary.INSTANCE.inateck_scanner_ble_init(callback);
-        return status;
+    String init() {
+        String result = CLibrary.INSTANCE.inateck_scanner_ble_init();
+        return result;
+    }
+
+    String setDiscoverCallback(EventCallback callback) {
+        String result = CLibrary.INSTANCE.inateck_scanner_ble_set_discover_callback(callback);
+        return result;
     }
     
-    int destroy() {
-        int status = CLibrary.INSTANCE.inateck_scanner_ble_destroy();
-        return status;
+    String waitAvailable() {
+        String result = CLibrary.INSTANCE.inateck_scanner_ble_wait_available();
+        return result;
     }
 
-    int startScan() {
-        int status = CLibrary.INSTANCE.inateck_scanner_ble_start_scan();
-        return status;
+    String startScan() {
+        String result = CLibrary.INSTANCE.inateck_scanner_ble_start_discover();
+        return result;
     }
 
-    int stopScan() {
-        int status = CLibrary.INSTANCE.inateck_scanner_ble_stop_scan();
+    String stopScan() {
+        String status = CLibrary.INSTANCE.inateck_scanner_ble_stop_discover();
         return status;
     }
 
@@ -30,99 +65,159 @@ public class ScannerBle {
         return devices;
     }
 
-    String connect(String mac, EventCallback callback) {
-        String result = CLibrary.INSTANCE.inateck_scanner_ble_connect(mac, callback);
-        return result;
-    }
-
-    int auth(String mac) {
-        int status = CLibrary.INSTANCE.inateck_scanner_ble_auth(mac);
+    String connect(String devicdID) {
+        String status = CLibrary.INSTANCE.inateck_scanner_ble_connect(devicdID);
         return status;
     }
 
-    int disconnect(String mac) {
-        int status = CLibrary.INSTANCE.inateck_scanner_ble_disconnect(mac);
+    String checkCommunication(String devicdID) {
+        String status = CLibrary.INSTANCE.inateck_scanner_ble_check_communication(devicdID);
         return status;
     }
 
-    String getBattery(String mac) {
-        String battery = CLibrary.INSTANCE.inateck_scanner_ble_get_battery(mac);
-        return battery;
-    }
-
-    String getHardwareVersion(String mac) {
-        String version = CLibrary.INSTANCE.inateck_scanner_ble_get_hardware_version(mac);
-        return version;
-    }
-
-    String getSoftwareVersion(String mac) {
-        String version = CLibrary.INSTANCE.inateck_scanner_ble_get_software_version(mac);
-        return version;
-    }
-
-    String getSettingInfo(String mac) {
-        String info = CLibrary.INSTANCE.inateck_scanner_ble_get_setting_info(mac);
-        return info;
-    }
-
-    String setSettingInfo(String mac, String cmd) {
-        String result = CLibrary.INSTANCE.inateck_scanner_ble_set_setting_info(mac, cmd);
-        return result;
-    }
-
-    int setName(String mac, String name) {
-        int status = CLibrary.INSTANCE.inateck_scanner_ble_set_name(mac, name);
+    String auth(String devicdID) {
+        String status = CLibrary.INSTANCE.inateck_scanner_ble_auth(devicdID);
         return status;
     }
 
-    int setTime(String mac, long time) {
-        int status = CLibrary.INSTANCE.inateck_scanner_ble_set_time(mac, time);
+    String setCodeCallback(String devicdID, EventCallback callback) {
+        String status = CLibrary.INSTANCE.inateck_scanner_ble_set_code_callback(devicdID, callback);
         return status;
     }
 
-    int inventoryClearCache(String mac) {
-        int status = CLibrary.INSTANCE.inateck_scanner_ble_inventory_clear_cache(mac);
+    String setDisconnectCallback(String devicdID, EventCallback callback) {
+        String status = CLibrary.INSTANCE.inateck_scanner_ble_set_disconnect_callback(devicdID, callback);
         return status;
     }
 
-    int inventoryUploadCache(String mac) {
-        int status = CLibrary.INSTANCE.inateck_scanner_ble_inventory_upload_cache(mac);
+    String disconnect(String devicdID) {
+        String status = CLibrary.INSTANCE.inateck_scanner_ble_disconnect(devicdID);
         return status;
     }
 
-    int inventoryUploadCacheNumber(String mac) {
-        int status = CLibrary.INSTANCE.inateck_scanner_ble_inventory_upload_cache_number(mac);
+    String getBattery(String devicdID) {
+        String status = CLibrary.INSTANCE.inateck_scanner_ble_get_battery(devicdID);
         return status;
     }
 
-    int reset(String mac) {
-        int status = CLibrary.INSTANCE.inateck_scanner_ble_reset(mac);
+    String getHardwareVersion(String devicdID) {
+        String status = CLibrary.INSTANCE.inateck_scanner_ble_get_hardware_version(devicdID);
         return status;
     }
 
-    int restart(String mac) {
-        int status = CLibrary.INSTANCE.inateck_scanner_ble_restart(mac);
+    String beeOrShake(String devicdID) {
+        String status = CLibrary.INSTANCE.inateck_scanner_ble_bee_or_shake(devicdID);
         return status;
     }
 
-    int closeAllCode(String mac) {
-        int status = CLibrary.INSTANCE.inateck_scanner_ble_close_all_code(mac);
+    String setBee(String devicdID, int voiceTime, int silentTime, int count) {
+        String status = CLibrary.INSTANCE.inateck_scanner_set_bee(devicdID, voiceTime, silentTime, count);
         return status;
     }
 
-    int openAllCode(String mac) {
-        int status = CLibrary.INSTANCE.inateck_scanner_ble_open_all_code(mac);
+    String setLed(String devicdID, int color, int lightTime, int darkTime, int count) {
+        String status = CLibrary.INSTANCE.inateck_scanner_set_led(devicdID, color, lightTime, darkTime, count);
         return status;
     }
 
-    int resetAllCode(String mac) {
-        int status = CLibrary.INSTANCE.inateck_scanner_ble_reset_all_code(mac);
+    String getSoftwareVersion(String devicdID) {
+        String status = CLibrary.INSTANCE.inateck_scanner_ble_get_software_version(devicdID);
         return status;
     }
 
-    String getSdkVersion() {
-        String version = CLibrary.INSTANCE.inateck_scanner_ble_sdk_version();
-        return version;
+    String getMac(String devicdID) {
+        String status = CLibrary.INSTANCE.inateck_scanner_ble_get_mac(devicdID);
+        return status;
+    }
+
+    String getSettingInfo(String devicdID, InateckDeviceType deviceType) {
+        String status = CLibrary.INSTANCE.inateck_scanner_ble_get_setting_info(devicdID, deviceType.getValue());
+        return status;
+    }
+
+    String setSettingInfo(String devicdID, String cmd, InateckDeviceType deviceType) {
+        String status = CLibrary.INSTANCE.inateck_scanner_ble_set_setting_info(devicdID, cmd, deviceType.getValue());
+        return status;
+    }
+
+    String setName(String devicdID, String name) {
+        String status = CLibrary.INSTANCE.inateck_scanner_ble_set_name(devicdID, name);
+        return status;
+    }
+
+    String setTime(String devicdID, long name) {
+        String status = CLibrary.INSTANCE.inateck_scanner_ble_set_time(devicdID, name);
+        return status;
+    }
+
+    String inventoryClearCache(String devicdID) {
+        String status = CLibrary.INSTANCE.inateck_scanner_ble_inventory_clear_cache(devicdID);
+        return status;
+    }
+
+    String inventoryUploadCache(String devicdID) {
+        String status = CLibrary.INSTANCE.inateck_scanner_ble_inventory_upload_cache(devicdID);
+        return status;
+    }
+
+    String inventoryUploadCacheNumber(String devicdID) {
+        String status = CLibrary.INSTANCE.inateck_scanner_ble_inventory_upload_cache_number(devicdID);
+        return status;
+    }
+
+    String reset(String devicdID) {
+        String status = CLibrary.INSTANCE.inateck_scanner_ble_reset(devicdID);
+        return status;
+    }
+
+    String restart(String devicdID) {
+        String status = CLibrary.INSTANCE.inateck_scanner_ble_restart(devicdID);
+        return status;
+    }
+
+    String closeAllCode(String devicdID) {
+        String status = CLibrary.INSTANCE.inateck_scanner_ble_close_all_code(devicdID);
+        return status;
+    }
+
+    String openAllCode(String devicdID) {
+        String status = CLibrary.INSTANCE.inateck_scanner_ble_open_all_code(devicdID);
+        return status;
+    }
+
+    String resetAllCode(String devicdID) {
+        String status = CLibrary.INSTANCE.inateck_scanner_ble_reset_all_code(devicdID);
+        return status;
+    }
+
+    String getPrefix(String devicdID) {
+        String status = CLibrary.INSTANCE.inateck_scanner_ble_get_prefix(devicdID);
+        return status;
+    }
+
+    String setPrefix(String devicdID, byte[] prefix, int prefixLen) {
+        String status = CLibrary.INSTANCE.inateck_scanner_ble_set_prefix(devicdID, prefix, prefixLen);
+        return status;
+    }
+
+    String getSuffix(String devicdID) {
+        String status = CLibrary.INSTANCE.inateck_scanner_ble_get_suffix(devicdID);
+        return status;
+    }
+
+    String setSuffix(String devicdID, byte[] suffix, int suffixLen) {
+        String status = CLibrary.INSTANCE.inateck_scanner_ble_set_suffix(devicdID, suffix, suffixLen);
+        return status;
+    }
+
+    String sdkVersion() {
+        String status = CLibrary.INSTANCE.inateck_scanner_ble_sdk_version();
+        return status;
+    }
+
+    int setDebug(int is_debug) {
+        int status = CLibrary.INSTANCE.inateck_scanner_ble_set_debug(is_debug);
+        return status;
     }
 
 }
